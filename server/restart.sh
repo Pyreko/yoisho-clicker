ab14652ab9ebec5263f5352edeb6066597a767b8
Pyreko <25498386+Pyreko@users.noreply.github.com>
2023-03-06 18:38:50 -0500

    script

--- server/restart.sh
+++ server/restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
-pkill -2 yoisho-server
-while pgrep -u $UID -x yoisho-server >/dev/null; do sleep 1; done
-mkdir -p ~/yc-server
-cp ./target/release/yoisho-server ~/yc-server/yoisho-server
-cp -r ./assets ~/yc-server/
-cd ~/yc-server
+pkill -2 yoisho-server;
+while pgrep -u $UID -x yoisho-server >/dev/null; do sleep 1; done;
+mkdir -p $HOME/yc-server;
+cp ./target/release/yoisho-server $HOME/yc-server/yoisho-server;
+cp -r ./assets $HOME/yc-server/;
+cd $HOME/yc-server;
 nohup ./yoisho-server > /dev/null 2>&1 &
