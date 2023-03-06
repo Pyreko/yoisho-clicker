cf9528f72b3cc47ef1f22d2c48eb26b7db0c9196
Pyreko <25498386+Pyreko@users.noreply.github.com>
2023-03-06 18:28:38 -0500

    binaries and paths

--- server/restart.sh
+++ server/restart.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-pkill -2 yoisho-clicker-server
-while pgrep -u $UID -x yoisho-clicker-server >/dev/null; do sleep 1; done
+pkill -2 yoisho-server
+while pgrep -u $UID -x yoisho-server >/dev/null; do sleep 1; done
 mkdir -p ~/yc-server
-cp ./target/release/yoisho-clicker-server ~/yc-server/yoisho-clicker-server
+cp ./target/release/yoisho-server ~/yc-server/yoisho-server
 cp -r ./assets ~/yc-server/
 cd ~/yc-server
-nohup ./yoisho-clicker-server > /dev/null 2>&1 &
+nohup ./yoisho-server > /dev/null 2>&1 &
